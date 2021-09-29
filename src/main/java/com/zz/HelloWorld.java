1b0aed375ea0380ee8a2fb6901e747212664f24f
zhoujian43 <364040115@qq.com>
2021-09-29 09:51:38 +0800

    hot-fix修改了代码

--- src/main/java/com/zz/HelloWorld.java
+++ src/main/java/com/zz/HelloWorld.java
@@ -6,5 +6,6 @@ package com.zz;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("世界, 你好");
+        System.out.println("hot-fix在9行添加的");
     }
 }
