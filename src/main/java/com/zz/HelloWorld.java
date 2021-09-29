fe2c65cb0de359ab98550ce4b635154396dc8742
zhoujian43 <364040115@qq.com>
2021-09-29 09:59:45 +0800

    master在10修改了代码

--- src/main/java/com/zz/HelloWorld.java
+++ src/main/java/com/zz/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("世界, 你好");
         System.out.println("hot-fix在9行添加的");
+        System.out.println("master在10修改了代码");
         System.out.println("hot-fix在10行添加的");
     }
 }
